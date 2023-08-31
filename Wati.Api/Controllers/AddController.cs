8198642c5d0d6db030b9abba3e35f2abd3c92f07
nehareeka <nehar2ka@gmail.com>
2023-08-31 16:00:07 +0530

    Added an http post method

---
+++ Wati.Api/Controllers/AddController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wati.Api.Models;
+
+namespace Wati.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddController : ControllerBase
+    {
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        /// <param name="request">Two integer numbers</param>
+        /// <returns>Sum of the input</returns>
+        [HttpPost]
+        public IActionResult Add([FromBody] RequestData request)
+        {
+            int num1 = request.Num1;
+            int num2 = request.Num2;
+            int sum = num1 + num2;
+            return Ok(sum);
+        }
+    }
+}
---
+++ Wati.Api/Models/RequestData.cs
@@ -0,0 +1,8 @@
+﻿namespace Wati.Api.Models
+{
+    public class RequestData
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
