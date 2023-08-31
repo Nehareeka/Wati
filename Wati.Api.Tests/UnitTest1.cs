237b2676b044e014c8d727a1fea9499551a78902
nehareeka <nehar2ka@gmail.com>
2023-08-31 16:59:15 +0530

    Frontend project setup with index page - displays two input fields and a button - computes sum and displays
    ToDo: send request to backend

---
+++ Wati.Api.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Wati.Api.Controllers;
+using Wati.Api.Models;
+using Xunit;
+
+namespace Wati.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddTwoNumbers_ReturnsSum()
+        {
+            //Arrange
+            RequestData data = new RequestData { Num1 = 1, Num2 = 2 };
+
+            //Act
+            AddController addCtrl = new AddController();
+            var result = addCtrl.Add(data) as ActionResult;
+
+            //Assert
+            int expected = 3;
+            Assert.IsType<OkResult>(result);
+            Assert.Equal(expected, result.);
+        }
+
+        [Fact]
+        public void AddTwoNumbers_ReturnsSum()
+        {
+            //Arrange
+            RequestData data = new RequestData { Num1 = 1, Num2 = 2 };
+
+            //Act
+            AddController addCtrl = new AddController();
+            var result = addCtrl.Add(data) as ActionResult;
+
+            //Assert
+            int expected = 3;
+            Assert.IsType<OkResult>(result);
+            Assert.Equal(expected, result.);
+        }
+    }
+}
---
+++ Wati.Web/wwwroot/js/app.js
@@ -0,0 +1,8 @@
+﻿var url = "https://localhost:7002";
+
+function add() {
+    var a = (Number)(document.getElementById("num1").value);
+    var b = (Number)(document.getElementById("num2").value);
+    var sum = (Number)(a + b);
+    document.getElementById("result").innerText = sum;
+}
